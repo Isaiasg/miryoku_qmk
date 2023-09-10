4fd1b8d18e70b1fb7bd474d3fa54d3d80d7c70fe
isaiasg <siderevs@gmail.com>
2023-09-10 08:16:19 -0400

    added info.json

--- keyboards/absolem/rules.mk
+++ keyboards/absolem/rules.mk
@@ -10,3 +10,4 @@ NKRO_ENABLE = yes            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+LTO_ENABLE = yes
