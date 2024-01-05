a6e330b463f898348dbb6567a0ac38827900a54b
isaiasg <siderevs@gmail.com>
2024-01-05 09:38:38 -0500

    changed diode director

--- keyboards/ffkb_lite/config.h
+++ keyboards/ffkb_lite/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5  /* 5 is default */
